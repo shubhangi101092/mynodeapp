e8c53d1d298c0436dc50762f3a90e8bc80559ab3
nishant.garg@loginworks.com <nishant.garg@loginworks.com>
2018-05-28 17:40:08 +0530

    crud

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.set("view engine", "ejs");
 //middlewares
 app.use(express.static(path.join(__dirname, "assets")))
 
-// app.use(bodyparser.json());
-// app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 //define allroutes
 app.get("/", function (req, res) {
@@ -28,6 +28,34 @@ app.get("/users", function (req, res) {
     res.status = 200;
     res.render('users', { users: usersdata });
 });
+
+app.get("/users/add", function (req, res) {
+    res.render('adduser');
+});
+
+app.get("/users/:id", function (req, res) {
+    var user = usersdata.find(user => user.id == req.params.id);
+    res.render('user', { user: user });
+});
+
+app.get("/edit/:id", function (req, res) {
+    var user = usersdata.find(user => user.id == req.params.id);
+    res.render('edit', { user: user });
+});
+app.post("/edit/:id",function(req,res){
+    var user = usersdata.find(user => user.id == req.params.id);
+    
+   
+})
+app.post("/users",function(req, res){
+    var user= {};
+    var body = req.body;
+    user = body;
+    user.id = usersdata.length + 1;
+    usersdata.push(user);
+    res.redirect("/users");
+});
+
 
 app.get("*", function (req, res) {
     res.status = 200;
