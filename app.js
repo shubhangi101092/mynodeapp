0795c596f2cca4c357c6b8177042b4525bcf0dba
nishant.garg@loginworks.com <nishant.garg@loginworks.com>
2018-05-29 18:32:28 +0530

    edit delete

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 var express = require("express"),
     ejs = require("ejs"),
     path = require("path"),
+    methodOverride = require("method-override"),
     bodyparser = require("body-parser");
-
+mysql = require('mysql');
 //initialize server/app
 var app = express();
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodeapp'
+});
+var usersdata = [];
+var reslt;
+connection.connect();
 
-var usersdata = require("./store/users.js");
+
 
 // change server settings
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //middlewares
-app.use(express.static(path.join(__dirname, "assets")))
-
+app.use(express.static(path.join(__dirname, "assets")));
+app.use(methodOverride('method'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
@@ -25,8 +35,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/users", function (req, res) {
-    res.status = 200;
-    res.render('users', { users: usersdata });
+    connection.query('SELECT * from users', function (error, results, fields) {
+        if (error) throw error;
+        userdata = results;
+        console.log("USERS:", results);
+        res.status = 200;
+        res.render('users', { users: userdata });
+    });
+
 });
 
 app.get("/users/add", function (req, res) {
@@ -34,28 +50,68 @@ app.get("/users/add", function (req, res) {
 });
 
 app.get("/users/:id", function (req, res) {
-    var user = usersdata.find(user => user.id == req.params.id);
-    res.render('user', { user: user });
+    var id = req.params.id;
+    connection.query('SELECT * from users  WHERE id=?', [id], function (error, results, fields) {
+        if (error) throw error;
+        userdata = results;
+        console.log("USERS:", results);
+        res.status = 200;
+        res.render('user', { user: userdata[0] });
+    });
 });
 
 app.get("/edit/:id", function (req, res) {
-    var user = usersdata.find(user => user.id == req.params.id);
-    res.render('edit', { user: user });
+    var id = req.params.id;
+    connection.query('SELECT * from users  WHERE id=?', [id], function (error, results, fields) {
+        if (error) throw error;
+        userdata = results;
+        console.log("USERS:", results);
+        res.status = 200;
+        res.render('edit', { user: userdata[0] });
+    });
 });
-app.post("/edit/:id",function(req,res){
-    var user = usersdata.find(user => user.id == req.params.id);
-    
-   
+app.post("/update/:id", function (req, res) {
+    var id = req.params.id, user = req.body;
+    connection.query('UPDATE users SET name= ?,email=?,username=?,phone=? WHERE id=?',[user.name,user.email,user.username,user.phone,id], function (error, results, fields) {
+        if (error) throw error;
+        // userdata = results;
+        console.log(user);
+        // res.status = 200;
+  //   res.redirect("/users");
+    });
+   // res.json(user);
 })
-app.post("/users",function(req, res){
-    var user= {};
+app.post("/users", function (req, res) {
+    var user = {};
     var body = req.body;
     user = body;
-    user.id = usersdata.length + 1;
-    usersdata.push(user);
+    var sql = "INSERT INTO users SET ?";
+    connection.query(sql, user, function (err, result, fields) {
+        //  if (err) throw err;
+        // console.log("1 record inserted, ID: " + result.insertId);
+    });
     res.redirect("/users");
+    //user.id = usersdata.length + 1;
+    // usersdata.push(user);
+    // res.redirect("/users");
 });
 
+app.delete("/deluser/:id", (req, res) => {
+    var id = req.params.id;
+     connection.query('DELETE from users  WHERE id=?', [id], function (error, results, fields) {
+        if (error) throw error;
+       // userdata = results;
+        //console.log("USERS:", results);
+        res.status = 200;
+       res.redirect("/users");
+    });
+    //res.end('User with ' + id + ' deleted.');
+});
+
+app.put("/updateuser/:id", (req, res) => {
+    var id = req.params.id;
+    res.end('User with ' + id + ' deleted.');
+});
 
 app.get("*", function (req, res) {
     res.status = 200;
